27e1dccfe2b22819ba168c805ff87c65a783f284
hardikagl2004-rgb <hardikagl2004@gmail.com>
2026-04-24 02:07:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from reportlab.lib.units import inch
 try:
     import google.generativeai as genai
     AI_AVAILABLE = True
-    genai.configure(api_key="AIzaSyC1f9_r8BVDPqCrOK7k-yUoU1Ov7zHxR9I")
+    genai.configure(api_key="AIzaSyAiLS1ahjTfFmgyLBSQNESiPD3x8qSHpvA")
 except ImportError:
     AI_AVAILABLE = False
 
